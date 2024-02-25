6da44b2d4b35e6b158dc5207a970d5cb914dc6f4
dzkita <146095028+dzkita@users.noreply.github.com>
2024-02-25 19:29:41 -0300

    Non recursive version

--- main.rs
+++ main.rs
@@ -24,18 +24,52 @@ fn generate_tree(level: usize, counter: &mut i32) -> NodeRef<i32> {
         Some(Box::new(node))
     }
 }
-
-fn print_tree<T: Display>(root: &NodeRef<T>, level: usize) {
+fn visit_nodes<T: Display>(root: &NodeRef<T>) {
     match root {
         Some(node) => {
-            print_tree(&node.left, level + 1);
-            for _ in 0..level {
-                print!("  ")
-            }
             println!("{}", node.value);
-            print_tree(&node.right, level + 1);
+            visit_nodes(&node.left);
+            visit_nodes(&node.right);
         }
         None => {}
+    }
+}
+enum Action<T, U> {
+    Call(T),
+    Handle(U),
+}
+fn print_tree_nonrec<T: Display>(root: &NodeRef<T>) {
+    let mut stack = Vec::<Action<(&NodeRef<T>, usize), (&T, usize)>>::new();
+
+    use Action::*;
+    stack.push(Call((root, 0)));
+    while let Some(action) = stack.pop() {
+        match action {
+            Call((root, level)) => {
+                if let Some(node) = root {
+                    stack.push(Call((&node.left, level + 1)));
+                    stack.push(Handle((&node.value, level)));
+                    stack.push(Call((&node.right, level + 1)));
+                }
+            }
+            Handle((value, level)) => {
+                for _ in 0..level {
+                    print!("  ");
+                }
+                println!("{}", value);
+            }
+        }
+    }
+}
+
+fn print_tree<T: Display>(root: &NodeRef<T>, level: usize) {
+    if let Some(node) = root {
+        print_tree(&node.right, level + 1);
+        for _ in 0..level {
+            print!("  ")
+        }
+        println!("{}", node.value);
+        print_tree(&node.left, level + 1);
     }
 }
 
@@ -55,5 +89,7 @@ fn main() {
     let tree = generate_tree(3, &mut counter);
     print_tree(&tree, 0);
     println!("------------------------");
-    print_tree(&invert_tree(tree),0);
+    // visit_nodes(&tree);
+    print_tree_nonrec(&tree);
+    // print_tree(&invert_tree(tree), 0);
 }
